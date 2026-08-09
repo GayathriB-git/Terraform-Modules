1d7c382e76ed1fe45bcbe656d5850f0ec4a0af1c
Gayathri Balu <gayathriananth55@gmail.com>
2026-08-09 20:57:39 +0530

    Add Helm release configuration for Argo CD
    This Terraform configuration defines a Helm release for Argo CD, specifying the repository, chart version, and service settings.

---
+++ Terraform/modules/argocd/argocd.tf
@@ -0,0 +1,28 @@
+resource "helm_release" "argocd" {
+    name       = "argocd"
+    repository = "https://argoproj.github.io/argo-helm"
+    chart      = "argo-cd"
+    version    = "5.43.0"
+    
+    namespace = "argocd"
+    
+    set {
+        name  = "server.service.type"
+        value = "LoadBalancer"
+    }
+    set {
+        name  = "server.service.ports.https.port"
+        value = "443"
+    }
+
+    set {
+    name  = "server.service.ports.https.targetPort"
+    value = "8080"
+    }
+    set {
+    name  = "server.insecure"
+    value = "true"
+   }
+  
+  
+}
